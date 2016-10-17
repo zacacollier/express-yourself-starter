2fb39359c232e31fb904ad9331af75e33510bdbf
zacacollier <zacacollier@gmail.com>
2016-10-17 15:32:49 -0500

    Project complete. Implemented Bootstrap for layout, added a route for project-specific styles.

--- app.js
+++ app.js
@@ -10,29 +10,45 @@ const ghAvatar = require('gh-avatar');
 const app = express();
 
 //Set the views directory
+app.use(express.static('css'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const image = ghAvatar('zacacollier')
-  .then(avatar => console.log(avatar))
+  .then(avatar => avatar)
  .catch(reason => console.log(`promise rejected: ${reason}`))
 ;
 console.log(image);
 
-
+const avatarUrl = 'https://avatars.githubusercontent.com/u/18710669?v=3';
 //Add a route that renders the index view
 app.get('/', (req, res, next) => {
-  res.render('index.ejs', { header: '<h1>Header</h1>',
-                            subheader: '<h2>Subheader</h2>',
-                            liheader: '<h3>List Header</h3>',
+  res.render('index.ejs', { header: '<h1>Zac Collier</h1>',
+                            subheader: '<h2>Full-Stack Developer-in-Training</h2>',
+                            liheader: '<h3>Skills:</h3>',
                             listItems: [
-                              'List Item',
-                              'List Item',
-                              'List Item',
-                              'List Item',
+                              'HTML / CSS',
+                              'Bootstrap',
+                              'Javascript ES6',
+                              'jQuery',
+                            ],
+                            learnHeader: '<h3>Learning:</h3>',
+                            learnItems: [
+                              'Express',
+                              'NodeJS',
+                              'React',
+                              'Redux',
+                              'Python 2 & 3'
+                            ],
+                            hobbiesHeader: "<h3>When I'm not studying I am:</h3>",
+                            hobbiesItems: [
+                              'Swimming',
+                              'Cooking',
+                              'Recording with Ableton',
+                              'Making patches with Reaktor'
                             ],
                             copy: '<p>Lorem ipsum castrati ergo sum</p>',
-                            avatar: image,
+                            avatar: avatarUrl
                             });
 })
 
